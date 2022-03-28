7d8f5b0db4dff42b489d4e6bd3e0cbbc1374a3dd
mihailkokoulin <kokoulin92@gmail.com>
2022-03-29 00:50:09 +0300

    sprint_1/increment_2: Added go-chi and rewrote the server using it

---
+++ internal/app/storage/storage_mock.go
@@ -0,0 +1,41 @@
+package storage
+
+import (
+	"errors"
+	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/helpers"
+)
+
+type MockStorage Storage
+
+var data = map[string]string{
+	"GMWJGSAPGA": "https://go.dev",
+	"TLMODYLUMG": "https://mail.google.com",
+	"UQDIWWMNPP": "https://practicum.yandex.ru",
+}
+
+func (s *MockStorage) GenerateMockData() {
+	for _, v := range data {
+		s.Save(v)
+	}
+}
+
+func (s *MockStorage) LinkBy(sl string) (string, error) {
+	link, ok := s.data[sl]
+	if !ok {
+		return link, errors.New("url not found")
+	}
+
+	return link, nil
+}
+
+func (s *MockStorage) Save(url string) (sl string) {
+	sl = string(helpers.RandomString(10))
+
+	if s.data == nil {
+		s.data = make(map[string]string)
+	}
+
+	s.data[sl] = url
+
+	return
+}
