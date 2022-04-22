e44a4dac38163f1cd23f649af6bb59e51a27fe68
mihailkokoulin <kokoulin92@gmail.com>
2022-04-22 18:31:48 +0400

    sprint_3/increment_10: Added logs for debbuging

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/helpers/db"
 	_ "github.com/jackc/pgx/stdlib"
 
 	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/configs"
@@ -17,14 +19,14 @@ func main() {
 
 	serv := server.New(c.ServerAddress, c)
 
-	//conn, err := db.New(c.DatabaseDSN)
-	//if err != nil {
-	//	log.Println("Closing connect to db")
-	//	err := conn.Close()
-	//	if err != nil {
-	//		log.Println("Closing don't close")
-	//	}
-	//}
+	conn, err := db.New(c.DatabaseDSN)
+	if err != nil {
+		log.Println("Closing connect to db")
+		err := conn.Close()
+		if err != nil {
+			log.Println("Closing don't close")
+		}
+	}
 
 	serv.Start()
 }
