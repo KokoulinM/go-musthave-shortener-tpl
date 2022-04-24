46f8134b1fa346f08ae0ad04b82237636b3d6b2f
mihailkokoulin <kokoulin92@gmail.com>
2022-04-24 09:41:59 +0400

    sprint_3/increment_10: Added logs for debbuging

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
 	"syscall"
 
+	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/handlers/middlewares"
 	_ "github.com/jackc/pgx/stdlib"
+	"go.uber.org/zap"
 
 	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/configs"
 	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/database"
 	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/helpers/db"
 	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/router"
-	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/server"
 )
 
 func main() {
 	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
 	cfg := configs.New()
 
@@ -35,10 +39,24 @@ func main() {
 
 	handler := router.New(db, cfg)
 
-	serv := server.New(db, cfg.ServerAddress, handler, cfg)
+	//serv := server.New(db, cfg.ServerAddress, handler, cfg)
+
+	srv := &http.Server{
+		Addr:    cfg.ServerAddress,
+		Handler: middlewares.Conveyor(handler, middlewares.GzipMiddleware, middlewares.CookieMiddleware),
+	}
+
+	//shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	//
+	//defer shutdownCancel()
+	//
+	//if srv != nil {
+	//	_ = srv.Shutdown(shutdownCtx)
+	//}
 
 	go func() {
-		serv.Start()
+		log.Fatal("app error exit", zap.Error(http.ListenAndServe(srv.Addr, srv.Handler)))
+		//serv.Start()
 	}()
 
 	killSignal := <-interrupt
--- go.mod
+++ go.mod
@@ -9,6 +9,9 @@ require (
 	github.com/jackc/pgx v3.6.2+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
