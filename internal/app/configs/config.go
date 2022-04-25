885ae23ef50e4bb177fbc78470acb24c9211c28a
mihailkokoulin <kokoulin92@gmail.com>
2022-04-25 08:26:54 +0400

    sprint_3/increment_10: Large refactoring with support for multiple repositories

--- internal/app/configs/config.go
+++ internal/app/configs/config.go
@@ -11,7 +11,7 @@ import (
 
 type Config struct {
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
+	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8082"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
 	DatabaseDSN     string `env:"DATABASE_DSN"`
 	Key             []byte
