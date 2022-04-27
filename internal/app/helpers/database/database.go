3d2a9e7416c9ac289ade71b3bb3b6e9cc37d29d5
mihailkokoulin <kokoulin92@gmail.com>
2022-04-27 11:00:49 +0400

    sprint_3/increment_11: Minor changes after linting

--- internal/app/helpers/database/database.go
+++ internal/app/helpers/database/database.go
@@ -28,7 +28,9 @@ func Conn(driverName, dsn string) (*sql.DB, error) {
 
 func SetUpDataBase(db *sql.DB, ctx context.Context) error {
 	var extention string
+
 	query := db.QueryRowContext(ctx, "SELECT 'exists' FROM pg_extension WHERE extname='uuid-ossp';")
+
 	err := query.Scan(&extention)
 	if err != nil {
 		return err
@@ -39,16 +41,20 @@ func SetUpDataBase(db *sql.DB, ctx context.Context) error {
 		if err != nil {
 			return err
 		}
+
 		log.Println("Create EXTENSION")
 	}
+
 	sqlCreateDB := `CREATE TABLE IF NOT EXISTS urls (
 								id serial PRIMARY KEY,
 								user_id uuid DEFAULT uuid_generate_v4 (), 	
-								original_url VARCHAR NOT NULL, 
+								origin_url VARCHAR NOT NULL, 
 								short_url VARCHAR NOT NULL UNIQUE,
 								is_deleted BOOLEAN NOT NULL DEFAULT FALSE
 					);`
 	res, err := db.ExecContext(ctx, sqlCreateDB)
+
 	log.Println("Create table", err, res)
+
 	return nil
 }
