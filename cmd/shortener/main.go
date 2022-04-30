419c11c4a379a85c632b052d149d87635c0b36f5
mihailkokoulin <kokoulin92@gmail.com>
2022-04-30 10:19:01 +0400

    sprint_3/increment_13: Change project structure

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,12 +7,12 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/KokoulinM/go-musthave-shortener-tpl/cmd/shortener/configs"
+	"github.com/KokoulinM/go-musthave-shortener-tpl/cmd/shortener/database"
+	"github.com/KokoulinM/go-musthave-shortener-tpl/cmd/shortener/router"
 	_ "github.com/lib/pq"
 
-	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/configs"
 	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/handlers"
-	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/helpers/database"
-	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/router"
 	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/server"
 	"github.com/KokoulinM/go-musthave-shortener-tpl/internal/app/storages"
 )
